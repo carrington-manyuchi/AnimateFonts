e7e831b378cf2ade69db48be4767b66b3c11a581
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-11-26 21:31:37 +0200

    Initial Commit

---
+++ AnimateFonts/AnimateFontsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimateFontsApp.swift
+//  AnimateFonts
+//
+//  Created by Manyuchi, Carrington C on 2024/11/26.
+//
+
+import SwiftUI
+
+@main
+struct AnimateFontsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
